4601a5e570797967ac528435b604f10ef50f4e7e
liamjken <liamjken@gmail.com>
2023-07-27 18:43:40 -0400

    googlesheets api

--- src/views/RotateTest.vue
+++ src/views/RotateTest.vue
@@ -1,16 +1,18 @@
 <template>
-        <v-card flat class="ma-10">
-            <div class="credits-container">
-    <div class="credits-scroll">
-      <div class="credit-item" v-for="(credit, index) in duplicatedCredits" :key="index">
-        {{ credit }}
+    <v-card flat class="ma-10">
+      <div class="credits-container">
+        <div class="credits-scroll" ref="creditsScroll" @animationiteration="resetAnimation">
+          <div class="credit-item" v-for="(credit, index) in sheetInfo" :key="index">
+            {{ credit }}
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</v-card>
+    </v-card>
   </template>
   
   <script>
+import axios from 'axios';
+
   export default {
     data() {
       return {
@@ -20,13 +22,35 @@
           'Text 3',
           // Add more credit texts as needed
         ],
+        sheetInfo: 'test',
       };
     },
-    computed: {
-      duplicatedCredits() {
-        // Duplicate the credits array to create a seamless loop
-        return [...this.credits, ...this.credits];
+    mounted(){
+        this.getSheetData();
+    },
+    methods: {
+      resetAnimation() {
+        const scrollContainer = this.$refs.creditsScroll;
+        const containerHeight = scrollContainer.offsetHeight;
+        const contentHeight = scrollContainer.scrollHeight;
+  
+        // Check if the content is taller than the container, to avoid scrolling if not needed
+        if (contentHeight > containerHeight) {
+          scrollContainer.style.animation = 'none'; // Disable animation
+          scrollContainer.scrollTop = 0; // Reset to the top
+          scrollContainer.style.animation = null; // Re-enable animation
+        }
       },
+     async getSheetData() {
+        try {
+  const data = await axios.get('https://sheets.googleapis.com/v4/spreadsheets/1kg13BI9McArzA9fZSVYbtjAESytzzySsSXeve-sGD08/values/Job%20applications!B2%3AB?dateTimeRenderOption=FORMATTED_STRING&majorDimension=ROWS&valueRenderOption=FORMATTED_VALUE&key=AIzaSyCIv9NNmFsUsVwe3DY68-rYf-zpuRAxpB4')
+  this.sheetInfo =  data.data.values.flatMap(item => item);
+  console.log(JSON.stringify(data.data.values))
+} catch(error) {
+  alert(error)
+}
+        
+     }
     },
   };
   </script>
@@ -34,11 +58,13 @@
   <style>
   .credits-container {
     overflow: hidden;
-    height: 100px; /* Adjust the height based on your requirements */
+    height: 50px; /* Adjust the height based on your requirements */
   }
   
   .credits-scroll {
-    animation: scrollCredits 5s linear infinite;
+    animation: scrollCredits 10s linear infinite;
+    display: flex;
+    flex-direction: column;
   }
   
   @keyframes scrollCredits {
@@ -46,7 +72,7 @@
       transform: translateY(0);
     }
     100% {
-      transform: translateY(calc(-1 * (100% + 10px))); /* Adjust the value based on the number of credits */
+      transform: translateY(calc(-100% - 10px)); /* Adjust the value based on the number of credits and spacing */
     }
   }
   
