20ee0af1431134fdafbb8c10ded2afd225ecaa6d
liamjken <liamjken@gmail.com>
2023-07-28 11:49:05 -0400

    rotate Mockup

--- src/main.js
+++ src/main.js
@@ -11,9 +11,11 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import * as labsComponents from 'vuetify/labs/components'
 
 const vuetify = createVuetify({
     components,
+    labsComponents,
     directives,
     theme: {
         themes: {
--- src/views/RotateTest.vue
+++ src/views/RotateTest.vue
@@ -1,28 +1,71 @@
 <template>
-    <v-card flat class="ma-10">
-      <div class="credits-container">
-        <div class="credits-scroll" ref="creditsScroll" @animationiteration="resetAnimation">
+    <v-card class="ma-10">
+        <v-img class="align-center text-center"
+      src="https://www.rightroaddigital.com/wp-content/uploads/2023/07/full-image.png"
+      height="250px"
+      cover
+    ><h1 class="heading">Celtic Fests - 500 Club</h1>
+<h3>Thank you for all the generous donations!</h3>
+</v-img>
+      <div class="credits-container ma-10">
+        <div class="credits-scroll align-center" ref="creditsScroll" @animationiteration="resetAnimation">
           <div class="credit-item" v-for="(credit, index) in sheetInfo" :key="index">
-            {{ credit }}
+           <h2> {{ credit.name }} - {{ credit.amount }}</h2>
           </div>
         </div>
       </div>
+      <v-card-title>
+     Find your Name below!
+      <v-spacer></v-spacer>
+      <v-text-field
+      class="pb-5 pt-5"
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+    v-model:items-per-page="itemsPerPage"
+    :headers="headers"
+    :items="sheetInfo"
+    :search="search"
+    item-value="name"
+    class="elevation-1 pa-5"
+  ></v-data-table>
     </v-card>
   </template>
   
   <script>
 import axios from 'axios';
+import { VDataTable } from 'vuetify/labs/VDataTable'
+
 
   export default {
+    components: {
+    VDataTable,
+  },
     data() {
       return {
+        search: '',
+        itemsPerPage: 5,
+        headers: [
+          {
+            title: 'Name',
+            align: 'start',
+            sortable: true,
+            key: 'name',
+          },
+          { title: 'Amount', align: 'end', key: 'amount' },
+        ],
         credits: [
           'Text 1',
           'Text 2',
           'Text 3',
           // Add more credit texts as needed
         ],
-        sheetInfo: 'test',
+        sheetInfo: [],
       };
     },
     mounted(){
@@ -43,9 +86,15 @@ import axios from 'axios';
       },
      async getSheetData() {
         try {
-  const data = await axios.get('https://sheets.googleapis.com/v4/spreadsheets/1kg13BI9McArzA9fZSVYbtjAESytzzySsSXeve-sGD08/values/Job%20applications!B2%3AB?dateTimeRenderOption=FORMATTED_STRING&majorDimension=ROWS&valueRenderOption=FORMATTED_VALUE&key=AIzaSyCIv9NNmFsUsVwe3DY68-rYf-zpuRAxpB4')
-  this.sheetInfo =  data.data.values.flatMap(item => item);
-  console.log(JSON.stringify(data.data.values))
+  const data = await axios.get('https://sheets.googleapis.com/v4/spreadsheets/1pB0MFxWTrkM49JiMgpCcPba0iw2UowQIvJ1ko6QZF1A/values/details!A2%3AB?dateTimeRenderOption=FORMATTED_STRING&majorDimension=ROWS&valueRenderOption=FORMATTED_VALUE&key=AIzaSyCIv9NNmFsUsVwe3DY68-rYf-zpuRAxpB4')
+  this.sheetInfo = data.data.values.map((row) => {
+      return {
+        name: row[0],
+        amount: row[1],
+      };
+    });
+
+ // this.sheetInfo = data.data.values
 } catch(error) {
   alert(error)
 }
@@ -55,14 +104,14 @@ import axios from 'axios';
   };
   </script>
   
-  <style>
+  <style scoped>
   .credits-container {
     overflow: hidden;
     height: 50px; /* Adjust the height based on your requirements */
   }
   
   .credits-scroll {
-    animation: scrollCredits 10s linear infinite;
+    animation: scrollCredits 15s linear infinite;
     display: flex;
     flex-direction: column;
   }
@@ -79,5 +128,9 @@ import axios from 'axios';
   .credit-item {
     margin-bottom: 10px; /* Add spacing between each credit item */
     /* Add your desired CSS styles for each credit item */
+  }
+
+ h1, h3 {
+    color: white;
   }
   </style>
