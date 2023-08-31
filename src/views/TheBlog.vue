244c55dbaf495355f8095fcf89d50f8a868ad694
liamjken <liamjken@gmail.com>
2023-08-31 11:34:48 -0400

    update

--- src/views/TheBlog.vue
+++ src/views/TheBlog.vue
@@ -24,6 +24,32 @@ export default {
 try {
   const data = await axios.get(`https://rightroaddigital.com/wp-json/wp/v2/posts/${this.postId}`)
   this.mainContent = data.data.content.rendered;
+
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(this.mainContent, 'text/html');
+    const targetElement = doc.getElementsByClassName('wp-block-group');
+
+    for (const el of targetElement) {
+        const ClassNames = el.className
+        if (ClassNames.includes('is-nowrap') && ClassNames.includes('is-layout-flex')) {
+        const classNameArray = ClassNames.split(' ');
+        const wpContainerClass = classNameArray.find(name => name.startsWith('wp-container-'))
+
+        if (wpContainerClass) {
+        const styleTag = document.createElement('style');
+        styleTag.setAttribute('id', 'vue-block-support');
+        styleTag.textContent = `.${wpContainerClass}.${wpContainerClass}{flex-wrap:nowrap;}`;
+        document.body.appendChild(styleTag);
+        }
+
+        }
+    }
+
+
+    console.log('ClassName')
+    console.log(targetElement)
+
+
 } catch(error) {
   alert(error)
 }
